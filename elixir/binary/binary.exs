5a54d42777b8e460fcc9dfa93b3ea56a038558ec
Adrian Kumpf <adrian.kumpf@posteo.de>
2017-01-08 21:54:30 +0100

    Elixir update binary solution

--- elixir/binary/binary.exs
+++ elixir/binary/binary.exs
@@ -6,14 +6,10 @@ defmodule Binary do
   On errors returns 0.
   """
   @spec to_decimal(String.t) :: non_neg_integer
-  def to_decimal(str) when is_bitstring(str), do: str |> to_charlist |> to_decimal
-  def to_decimal(chars), do: to_decimal(chars, valid?(chars))
-  defp to_decimal(_, false), do: 0
-  defp to_decimal(bin, true), do: Enum.reduce(bin, 0, &shift/2)
-
-  defp shift(?0, acc), do: acc <<< 1
-  defp shift(?1, acc), do: acc <<< 1 ||| 1
-
-  defp valid?(chars), do: Enum.all?(chars, &(&1 in ?0..?1))
+  def to_decimal(str), do: to_decimal(str, 0)
+  defp to_decimal("0" <> tail, dec), do: to_decimal(tail, dec * 2)
+  defp to_decimal("1" <> tail, dec), do: to_decimal(tail, dec * 2 + 1)
+  defp to_decimal("", dec), do: dec
+  defp to_decimal(_, _), do: 0
 
 end
